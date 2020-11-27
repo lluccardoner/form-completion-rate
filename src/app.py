99a65d57cdda900ad7516bf824369b918c163b9a
Lluc Cardoner <lluccardoner@gmail.com>
2020-11-27 20:38:59 +0100

    Arg parser

--- src/app.py
+++ src/app.py
@@ -1,11 +1,24 @@
 from pyspark.sql import SparkSession
 
+import arg_parser
 import dataset
 
 if __name__ == "__main__":
+    """
+    App that performs the following tasks:
+    - Load a dataset
+    - Process the dataset
+    - Train a model
+    - Do predictions
+    """
     spark = SparkSession \
         .builder \
         .appName("form_complation_rate") \
         .getOrCreate()
 
-    df = dataset.load_dataset("")
+    args = arg_parser.get_args()
+
+    if args.debug:
+        print("Using debug mode")
+
+    df = dataset.load_dataset(spark, args.datasetPath, args.debug)
--- src/dataset.py
+++ src/dataset.py
@@ -2,14 +2,18 @@ from pyspark.sql import DataFrame
 from pyspark.sql import SparkSession
 
 
-def load_dataset(spark: SparkSession, path: str) -> DataFrame:
+def load_dataset(spark: SparkSession, path: str, debug: bool = False) -> DataFrame:
     """
     Load dataset from the given path
     and add the completion rate column
+    :param debug: if true, print debug info
     :param spark: SparkSession
     :param path: input path
     :return: pyspark DataFrame
     """
+
+    if debug:
+        print("Loading dataset from {}".format(path))
 
     df = spark.read.option("header", True).csv(path)
     df = df.withColumn("CR", df["submissions"] / df["views"])
